5e204389ded82545cd330ab889d507d588576072
dswersky <dswersky@gmail.com>
2022-09-26 13:58:13 -0500

    Added variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+### VARIABLES
+variable "project-id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-a"
+}
+
+variable "subnet-name" {
+  type = string
+  default = "subnet1"
+}
+
+variable "subnet-cidr" {
+  type = string
+  default = "10.127.0.0/20"
+}
+
+variable "private_google_access" {
+  type = bool
+  default = true
+}
