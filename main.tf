8be4dc47570e1fdcea108ac39fa8c7120a103618
dswersky <dswersky@gmail.com>
2022-10-04 14:41:49 -0500

    removed commented resources and output vars

--- main.tf
+++ main.tf
@@ -80,66 +80,6 @@ resource "google_compute_instance" "web-instances" {
   }
 }
 
-# # WEB1
-# resource "google_compute_instance" "web1" {
-#   name         = "web1"
-#   machine_type = var.environment_machine_type[var.target_environment]
-#   labels = {
-#     environment = var.environment_map[var.target_environment]
-#   }
-  
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#     }
-#   }
-
-#   network_interface {
-#     # A default network is created for all GCP projects
-#     network = data.google_compute_network.default.self_link
-#     subnetwork = google_compute_subnetwork.subnet-1.self_link
-#   }
-# }
-
-# ## WEB2
-# resource "google_compute_instance" "web2" {
-#   name         = "web2"
-#   machine_type = var.environment_machine_type[var.target_environment]
-#   labels = {
-#     environment = var.environment_map[var.target_environment]
-#   }
-  
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#     }
-#   }
-
-#   network_interface {
-#     network = data.google_compute_network.default.self_link
-#     subnetwork = google_compute_subnetwork.subnet-1.self_link
-#   }
-# }
-# ## WEB3
-# resource "google_compute_instance" "web3" {
-#   name         = "web3"
-#   machine_type = var.environment_machine_type[var.target_environment]
-#   labels = {
-#     environment = var.environment_map[var.target_environment]
-#   }
-  
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#     }
-#   }
-
-#   network_interface {
-#     network = data.google_compute_network.default.self_link
-#     subnetwork = google_compute_subnetwork.subnet-1.self_link
-#   }  
-# }
-
 ## DB
 resource "google_compute_instance" "mysqldb" {
   name         = "mysqldb"
--- outputs.tf
+++ outputs.tf
@@ -7,19 +7,6 @@ output "webserver-ips" {
     value = google_compute_instance.web-instances[*].network_interface[0].network_ip
 }
 
-
-# output "web1-private-ip" {
-#     value = google_compute_instance.web1.network_interface[0].network_ip
-# }
-
-# output "web2-private-ip" {
-#     value = google_compute_instance.web2.network_interface[0].network_ip
-# }
-
-# output "web3-private-ip" {
-#     value = google_compute_instance.web3.network_interface[0].network_ip
-# }
-
 output "db-private-ip" {
     value = google_compute_instance.mysqldb.network_interface[0].network_ip
 }
