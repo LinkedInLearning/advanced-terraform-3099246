61617e444486c3ead6926e45711679eac63f3720
dswersky <dswersky@gmail.com>
2022-10-10 15:45:20 -0500

    added serviceaccounts.tf

---
+++ serviceaccounts.tf
@@ -0,0 +1,11 @@
+## SERVICE ACCOUNTS
+module "service_accounts" {
+  source        = "terraform-google-modules/service-accounts/google"
+  project_id    = var.project-id
+  prefix        = "viewer-sa"
+  names         = ["dev", "qa","stage","prod"]
+  project_roles = [
+    "${var.project-id}=>roles/viewer",
+    "${var.project-id}=>roles/storage.objectViewer",
+  ]
+}
