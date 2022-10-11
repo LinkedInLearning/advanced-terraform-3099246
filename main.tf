da3c9ad30862cd95bd0acfbcc4e9de1c97c894c5
dswersky <dswersky@gmail.com>
2022-10-11 13:37:34 -0500

    added comment to replace project-id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform"
+  project = "advancedterraform" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
