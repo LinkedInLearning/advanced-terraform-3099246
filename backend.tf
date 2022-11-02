25d412737b1bc9ec399ca4b6ffe475ef5d0c9d75
dswersky <dswersky@gmail.com>
2022-11-02 10:31:07 -0500

    updated backend to refer to TF Cloud

--- backend.tf
+++ backend.tf
@@ -1,5 +1,11 @@
 terraform {
-  backend "gcs" {
-    prefix  = "terraform/state"
+  cloud {
+    organization = "AdvancedTerraform" # CHANGE TO YOUR ORG NAME
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+
+    workspaces {
+      name = "advanced-terraform-3099246"
+    }
   }
 }
+
