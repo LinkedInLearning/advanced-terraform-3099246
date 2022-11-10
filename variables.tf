ace73acd288728b3ff0b9324db03e26d37123057
dswersky <dswersky@gmail.com>
2022-11-10 16:20:58 -0600

    moved environment settings to terraform.auto.tfvars

--- variables.tf
+++ variables.tf
@@ -75,44 +75,4 @@ variable "environment_machine_type" {
 
 variable "environment_instance_settings" {
   type = map(any)
-  default = {
-    "DEV" = {
-      machine_type = "f1-micro"
-      labels = {
-        environment = "dev"
-      },
-      redis = {
-        name = "customer-id-cache",
-        tier = "STANDARD_HA",
-        memory_size_gb =1,
-        display_name = "Customer Data Cache"
-      }
-    },
-   "QA" = {
-      machine_type = "e2-micro"
-      labels = {
-        environment = "qa"
-      },
-      redis = {
-        name = "customer-id-cache",
-        tier = "STANDARD_HA",
-        memory_size_gb = 3,
-        display_name = "Customer Data Cache"
-      }
-    },
-    "STAGE" = {
-      machine_type = "e2-micro"
-      labels = {
-        environment = "stage"
-      },
-      redis = {}
-    },
-    "PROD" = {
-      machine_type = "e2-medium"
-      labels = {
-        environment = "prod"
-      },
-      redis = {}
-    }
-  }
 }
