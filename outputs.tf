8bbe09c48710e104d237798607fbd011168512c4
dswersky <dswersky@gmail.com>
2022-11-19 07:55:43 -0600

    added simple output variable

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 ### OUTPUTS ###
+output "subnet-gateway" {
+    value = google_compute_subnetwork.subnet-1.gateway_address
+}
+
+
 output "nginx-public-ip" {
     value = google_compute_instance.nginx_instance.network_interface[0].access_config[0].nat_ip
 }
