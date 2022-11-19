5426a9176a53213d490c3b040974000a2f4c093d
dswersky <dswersky@gmail.com>
2022-11-19 16:20:04 -0600

    updated names in serviceaccounts module to use for

--- serviceaccounts.tf
+++ serviceaccounts.tf
@@ -3,7 +3,7 @@ module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   project_id    = var.project-id
   prefix        = "viewer-sa"
-  names         = ["dev", "qa","stage","prod"]
+  names         = [for s in var.environment_list: lower(s)]
   project_roles = [
     "${var.project-id}=>roles/viewer",
     "${var.project-id}=>roles/storage.objectViewer",
