2e1868dc8e42fd4bae2720dda2b67d0cb83119ef
Ry <vividedsc@gmail.com>
2024-02-20 14:34:36 -0500

    updated mainTF file

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advancedterraform-414900" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
